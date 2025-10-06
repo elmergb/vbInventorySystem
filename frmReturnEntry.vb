f94b86d1fd042c172a6f9f0b8d11f49c13302ec3
Elmer <bantoyelmer2@gmail.com>
2025-10-06 23:17:37 +0800

    contuie tommorow returning

--- frmReturnEntry.vb
+++ frmReturnEntry.vb
@@ -5,6 +5,16 @@
     End Sub
 
     Private Sub btnReturnLog_Click(sender As System.Object, e As System.EventArgs) Handles btnReturnLog.Click
+        Dim cmd As Odbc.OdbcCommand
+        Dim borrowID As Integer = Val(frmBorrowerList.dgvBorrowerList.Item(1, frmBorrowerList.dgvBorrowerList.CurrentRow.Index).Value)
+        Dim ItemID As Integer = Val(frmBorrowerList.dgvBorrowerList.Item(0, frmBorrowerList.dgvBorrowerList.CurrentRow.Index).Value)
+        Dim qtyBorrowed As Integer = Val(frmBorrowerList.dgvBorrowerList.Item(4, frmBorrowerList.dgvBorrowerList.CurrentRow.Index).Value)
+        If Val(frmBorrowerList.Tag) = 0 Then
+            Try
+                cmd = New Odbc.OdbcCommand("UPDATE")
+            Catch ex As Exception
 
+            End Try
+        End If
     End Sub
 End Class
