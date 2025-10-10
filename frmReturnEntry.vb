7025020af9198fc2bc48b89ebe2b5d0f6c9a4a23
Elmer <bantoyelmer2@gmail.com>
2025-10-10 23:45:39 +0800

    break return

--- frmReturnEntry.vb
+++ frmReturnEntry.vb
@@ -1,4 +1,8 @@
 ﻿Public Class frmReturnEntry
+    Public BorrowID As Integer
+    Public ItemID As Integer
+    Public QtyBorrowed As Integer
+    Public Property ReturnIDs As Integer = 0
     Public Property returnID As Integer = 0
     Private Sub frmReturnEntry_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         cb_loader("SELECT * FROM tblitemlist", cbItemListR, "ItemName", "ItemID")
@@ -6,41 +10,44 @@
 
     Private Sub btnReturnLog_Click(sender As System.Object, e As System.EventArgs) Handles btnReturnLog.Click
         Dim cmd As Odbc.OdbcCommand
-        Dim borrowID As Integer = Val(frmBorrowerList.dgvBorrowerList.Item(1, frmBorrowerList.dgvBorrowerList.CurrentRow.Index).Value)
-        Dim ItemID As Integer = Val(frmBorrowerList.dgvBorrowerList.Item(0, frmBorrowerList.dgvBorrowerList.CurrentRow.Index).Value)
-        Dim qtyBorrowed As Integer = Val(frmBorrowerList.dgvBorrowerList.Item(4, frmBorrowerList.dgvBorrowerList.CurrentRow.Index).Value)
+        Dim itemID As Integer = Me.ItemID
+        Dim borrowID As Integer = Me.BorrowID
+        Dim qtyReturned As Integer = CInt(nupQuantityR.Value) ' The quantity being returned
         Dim currentQty As Integer = 0
-        If Val(frmBorrowerList.Tag) = 0 Then
-            Try
-                cmd = New Odbc.OdbcCommand("SELECT ItemQuantity FROM tblItemList WHERE ItemID = ?", con)
-                cmd.Parameters.AddWithValue("?", ItemID)
-                Dim result = cmd.ExecuteScalar
 
-                If result IsNot Nothing Then
-                    qtyBorrowed = CInt(result)
-                End If
+        Try
+            ' Get current stock quantity
+            cmd = New Odbc.OdbcCommand("SELECT ItemQuantity FROM tblItemList WHERE ItemID = ?", con)
+            cmd.Parameters.AddWithValue("?", itemID)
+            Dim result = cmd.ExecuteScalar
 
-                Dim allQty = currentQty + qtyBorrowed
+            If result IsNot Nothing Then
+                currentQty = CInt(result)
+            End If
 
-                cmd = New Odbc.OdbcCommand("UPDATE tblitemlist SET ItemQuantity = ?, ItemID = ?", con)
-                With cmd.Parameters
-                    .AddWithValue("?", allQty)
-                    .AddWithValue("?", ItemID)
-                End With
-                cmd.ExecuteNonQuery()
+            ' Add returned quantity to current stock
+            Dim newQty = currentQty + qtyReturned
 
-                ' (Optional) Update borrow status as "Returned"
-                cmd = New Odbc.OdbcCommand("UPDATE tblBorrowing SET Status = 'Returned' WHERE BorrowID = ?", con)
-                cmd.Parameters.AddWithValue("?", borrowID)
-                cmd.ExecuteNonQuery()
+            ' Update item quantity in stock
+            cmd = New Odbc.OdbcCommand("UPDATE tblitemlist SET ItemQuantity = ? WHERE ItemID = ?", con)
+            With cmd.Parameters
+                .AddWithValue("?", newQty)
+                .AddWithValue("?", itemID)
+            End With
+            cmd.ExecuteNonQuery()
 
-                MsgBox("Item successfully returned!", vbInformation)
+            ' Update borrow status as "Returned"
+            'cmd = New Odbc.OdbcCommand("UPDATE tblborrow SET Status = 'Returned' WHERE BorrowID = ?", con)
+            'cmd.Parameters.AddWithValue("?", borrowID)
+            'cmd.ExecuteNonQuery()
 
-                ' Reload DataGridView
-                data_loader("SELECT * FROM vw_borrowing", frmReturnList.dgvReturnList)
-            Catch ex As Exception
+            MsgBox("Item successfully returned!", vbInformation)
 
-            End Try
-        End If
+            ' Reload DataGridView
+            data_loader("SELECT * FROM vw_borrowing", frmReturnList.dgvReturnList)
+        Catch ex As Exception
+            MsgBox("Error: " & ex.Message, vbCritical)
+        End Try
+
     End Sub
 End Class
